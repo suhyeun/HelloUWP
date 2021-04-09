18f5931d89f80569fbaecfe2687ed7400f4e8526
KSH <ksh64520@naver.com>
2021-04-09 16:11:15 +0900

    초기 커밋.

---
+++ HelloUWP/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using HelloUWP.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace HelloUWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel ViewModel => DataContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
